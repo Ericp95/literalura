79d86b9ec025934db0824ff3db25c9ef743568e8
Ericp95 <ericp6960@gmail.com>
2024-05-22 09:14:50 -0300

    creando el dto de autores

---
+++ src/main/java/com/alura/literalura/model/dto/AutorDTO.java
@@ -0,0 +1,12 @@
+package com.alura.literalura.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record AutorDTO(
+        @JsonAlias(value = "name") String nombre,
+        @JsonAlias(value = "birth_year") int annoNacimiento,
+        @JsonAlias(value = "death_year")int annoMuerte
+
+) {
+}
